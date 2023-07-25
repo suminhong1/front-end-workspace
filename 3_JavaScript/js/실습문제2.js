fe896bfa3abb446f2064da65366b9a03b7e6a31d
suminhong1 <125255022+suminhong1@users.noreply.github.com>
2023-07-25 17:16:28 +0900

    2023-07-25

--- 3_JavaScript/js/실습문제2.js
+++ 3_JavaScript/js/실습문제2.js
@@ -6,9 +6,63 @@ function resizeHandler() {
     // 전체 스크롤 할 수 있는 범위 = 바디 전체 높이 - 윈도우 현재 창의 높이
 }
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     progressBar.style.width = (window.scrollY / maxScrollValue) * 100 + '%';
-   
+
 });
-window.addEventListener('resize',resizeHandler);
+window.addEventListener('resize', resizeHandler);
 resizeHandler();
+
+// navigation click event
+const nav = document.querySelector('nav');
+let currentNav;
+
+function navHandler(e) {
+    if (currentNav) {
+        currentNav.style.backgroundColor = 'transparent';
+        currentNav.style.color = 'black';
+    }
+    if (e.target !== e.currentTarget) {
+        console.log(e.target);
+        e.target.style.backgroundColor = 'black';
+        e.target.style.color = 'white';
+    }
+    currentNav = e.target;
+}
+nav.addEventListener('click', navHandler);
+
+// mouse wheel event
+const section1 = document.querySelector('#section1');
+const section2 = document.querySelector('#section2');
+const section3 = document.querySelector('#section3');
+
+let currentSection = section1;
+
+window.addEventListener('wheel', function(event) {
+    if(event.deltaY > 0) { // 휠을 아래로 내린 경우
+        if(currentSection === section1) {
+            window.scrollTo({top: section2.offsetTop, behavior: 'smooth'});
+            currentSection = section2;
+        } else if(currentSection === section2) {
+            window.scrollTo({top: section3.offsetTop, behavior: 'smooth'});
+            currentSection = section3;
+        }
+    } else { // 휠을 위로 올린 경우
+        if(currentSection === section3) {
+            window.scrollTo({top: section2.offsetTop, behavior: 'smooth'});
+            currentSection = section2;
+        } else if(currentSection === section2) {
+            window.scrollTo({top: section1.offsetTop, behavior: 'smooth'});
+            currentSection = section1;
+        }
+    }
+});
+
+// scroll - animation 살짝~
+window.addEventListener('scroll', function() {
+    if(section2.getBoundingClientRect().top === 0){
+        section2.children[0].classList.add('text-animation');
+    } else {
+        section2.children[0].classList.remove('text-animation');
+    }
+});
