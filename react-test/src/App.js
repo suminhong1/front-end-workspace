671988df45b02eb774d196624f58758e8bb0ba11
suminhong1 <125255022+suminhong1@users.noreply.github.com>
2023-10-20 18:59:11 +0900

    00

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,24 +1,58 @@
-import React from "react";
-import Rating from "@mui/material/Rating";
-import Box from "@mui/material/Box";
+import React, { useState } from "react";
 
-function UserRating({ rating }) {
+function SelectForm() {
+  const [firstSelectOption, setFirstSelectOption] = useState("");
+  const [secondSelectOption, setSecondSelectOption] = useState("");
+  const [subOptions, setSubOptions] = useState([]);
+
+  const handleFirstSelectChange = (event) => {
+    const selectedValue = event.target.value;
+    setFirstSelectOption(selectedValue);
+
+    if (selectedValue === "option1") {
+      setSubOptions(["subOption1", "subOption2", "subOption3"]);
+    } else if (selectedValue === "option2") {
+      setSubOptions(["subOption4", "subOption5"]);
+    } else {
+      setSubOptions([]);
+    }
+
+    setSecondSelectOption("");
+  };
+
+  const handleButtonClick = () => {
+    console.log("첫 번째 선택: ", firstSelectOption);
+    console.log("두 번째 선택: ", secondSelectOption);
+  };
+
   return (
-    <Box display="flex" alignItems="center">
-      <Rating name="user-rating" value={rating} readOnly />
-      <span>{rating}</span>
-    </Box>
+    <div>
+      <div>
+        <label>첫 번째 선택: </label>
+        <select value={firstSelectOption} onChange={handleFirstSelectChange}>
+          <option value="">선택하세요</option>
+          <option value="option1">옵션 1</option>
+          <option value="option2">옵션 2</option>
+          <option value="option3">옵션 3</option>
+        </select>
+      </div>
+      <div>
+        <label>두 번째 선택: </label>
+        <select
+          value={secondSelectOption}
+          onChange={(event) => setSecondSelectOption(event.target.value)}
+        >
+          <option value="">선택하세요</option>
+          {subOptions.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button onClick={handleButtonClick}>값 출력</button>
+    </div>
   );
 }
 
-// 사용자의 평점이 3점인 경우
-// function App() {
-//   return (
-//     <div>
-//       <h2>사용자의 평점:</h2>
-//       <UserRating rating={3} />
-//     </div>
-//   );
-// }
-
-export default UserRating;
+export default SelectForm;
---
+++ react-test/src/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SelectForm from "./SelectForm"; // SelectForm 컴포넌트를 불러옴
+
+function test() {
+  return (
+    <div>
+      <h1>리액트 연동된 두 개의 Select 요소 예제</h1>
+      <SelectForm />
+    </div>
+  );
+}
+
+export default test;
